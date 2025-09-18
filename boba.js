eb1d47bd6cc89e74b1ebb4e81fc9c3048b561630
Umar-Kad <umarkhon.kadirov@gmail.com>
2025-09-18 22:31:54 +0000

    My Cool Website

--- boba.js
+++ boba.js
@@ -1,12 +1,20 @@
-const number = document.getElementById('numValue')
-const button = document.getElementById('color-change')
+let number = document.getElementById('numValue')
+let button = document.getElementById('color-change')
+let thumbimage = document.getElementById('thumb')
+let thumbtext = document.getElementById('thumbWord')
+
 
 let numCount = 0
 
-function addNum()  {
-    button.addEventListener('click', () => {
-        numCount++
-    })
 
-    number.textContent = numCount
+button.addEventListener("click", () => {
+    numCount += 1;
+    number.textContent = numCount;
+    if(numCount == 10) {
+        document.body.style.backgroundColor = 'red'
+        thumbimage.style.display = "block"
+        thumbtext.style.display = "block"
 };
+});
+
+
