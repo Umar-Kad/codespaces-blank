79e07d5e6dbd899899bc31c4f00e178756218190
Umar-Kad <umarkhon.kadirov@gmail.com>
2025-09-18 21:55:00 +0000

    commit

--- boba.js
+++ boba.js
@@ -1,12 +1,12 @@
-let Number = document.getElementById('numValue')
-let button = document.getElementById('color-change')
+const number = document.getElementById('numValue')
+const button = document.getElementById('color-change')
 
 let numCount = 0
 
-function addNum() {
-    button.addEventListener('click'); {
-        numCount += 1
-    }
+function addNum()  {
+    button.addEventListener('click', () => {
+        numCount++
+    })
 
-    Number.textContent = numCount
-}
+    number.textContent = numCount
+};
